d1a55f4aabf756e5e748563aa5d5a2d013a48e5a
Piotr Machura <piotr.machura16@gmail.com>
2021-03-17 21:21:02 +0100

    Another test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import package1.Test1;
+import package2.Test;
 
 /**
  * The class Main
@@ -10,8 +11,10 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+
         System.out.println("HeLL0!");
-        
+        System.out.println(Test.getTrue());
+
         Test1 obiekTest1 = new Test1(4, 12);
         System.out.println(obiekTest1.returnVectorLength());
     }
---
+++ src/main/java/package2/Test.java
@@ -0,0 +1,7 @@
+package package2;
+
+public class Test {
+    public static boolean getTrue() {
+        return false;
+    }
+}
