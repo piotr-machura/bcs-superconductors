d7cfe70b963bd82965b1c517a5a77d766b665ce7
Piotr Machura <piotr.machura16@gmail.com>
2021-03-17 16:42:09 +0100

    Create project

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * The class Main
+ *
+ * @Author Bartosz Biernacki, Piotr Machura
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
