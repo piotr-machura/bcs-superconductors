271879168a5b0b8afd36c32fdf22c8d99632b351
Piotr Machura <piotr.machura16@gmail.com>
2021-03-20 09:05:21 +0100

    Add JTransforms as maven dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,11 @@
  *
  * @Author Bartosz Biernacki, Piotr Machura
  **/
+import org.jtransforms.fft.DoubleFFT_1D;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        DoubleFFT_1D fft = new DoubleFFT_1D(10);
+        System.out.println(fft);
     }
 }
