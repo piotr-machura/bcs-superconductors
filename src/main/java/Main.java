c0a1cf57c4cb3947064c77e257a4c832b11621e1
BartoszBiernacki <bartosz.biernacki@int.pl>
2021-03-30 23:28:54 +0200

    Finding fixed point example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
+import staticFunctions.FixedPoint;
+
+
 /**
  * The class Main
  *
  * @Author Bartosz Biernacki, Piotr Machura
  **/
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
+        System.out.println(FixedPoint.findRoot(0.000001));
     }
+
 }
