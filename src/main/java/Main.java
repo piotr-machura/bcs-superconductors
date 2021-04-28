bf70cef6f91f2ad8c5c1d2b7a272d0df8192e49e
Piotr Machura <piotr.machura16@gmail.com>
2021-04-28 22:22:35 +0200

    Add fermi energy graph

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,8 @@ public class Main {
     final static double dx = 1e-4;
     final static int nX = 6 * (int) 1e1;
     static ArrayList<Double> ks = establishKs();
+    static double nParticles = 0;
+    static double mass = 1;
 
     /**
      * Establishes the wave vector grid used in the calculations. Only needed once
@@ -61,51 +63,47 @@ public class Main {
     /**
      * Calculate the kinetic energy from wave vector k.
      *
-     * @param k    the wave vector.
-     * @param mass the mass of the particle.
+     * @param k the wave vector.
      * @return the epsilonK.
      **/
-    static double epsilonK(double k, double mass) {
+    static double epsilonK(double k) {
         return (h_bar * h_bar * k * k) / (2 * mass);
     }
 
     /**
      * Calculate the square of the u wave function.
      *
-     * @param k    the wave vector.
-     * @param Ek   the energy associated with wave vector k.
-     * @param mass the mass of the particle.
-     * @param mu   the chemical potential.
+     * @param k  the wave vector.
+     * @param Ek the energy associated with wave vector k.
+     * @param mu the chemical potential.
      * @return the uK^2
      **/
-    static double uK2(double k, double Ek, double mass, double mu) {
-        return 0.5 * (1.0 + ((epsilonK(k, mass) - mu) / Ek));
+    static double uK2(double k, double Ek, double mu) {
+        return 0.5 * (1.0 + ((epsilonK(k) - mu) / Ek));
     }
 
     /**
      * Calculate the square of the v wave function.
      *
-     * @param k    the wave vector.
-     * @param Ek   the energy associated with wave vector k.
-     * @param mass the mass of the particle.
-     * @param mu   the chemical potential.
+     * @param k  the wave vector.
+     * @param Ek the energy associated with wave vector k.
+     * @param mu the chemical potential.
      * @return the vK^2.
      **/
-    static double vK2(double k, double Ek, double mass, double mu) {
-        return 0.5 * (1.0 - ((epsilonK(k, mass) - mu) / Ek));
+    static double vK2(double k, double Ek, double mu) {
+        return 0.5 * (1.0 - ((epsilonK(k) - mu) / Ek));
     }
 
     /**
      * Calculate the product of the u and v wave functions.
      *
-     * @param k    the wave vector.
-     * @param Ek   the energy associated with wave vector k.
-     * @param mass the mass of the particle.
-     * @param mu   the chemical potential.
+     * @param k  the wave vector.
+     * @param Ek the energy associated with wave vector k.
+     * @param mu the chemical potential.
      * @return the product uk*vk.
      **/
-    static double uKvK(double k, double Ek, double mass, double mu) {
-        double underSqrt = 0.25 * (1.0 - ((epsilonK(k, mass) - mu) / Ek)) * (1.0 + ((epsilonK(k, mass) - mu) / Ek));
+    static double uKvK(double k, double Ek, double mu) {
+        double underSqrt = 0.25 * (1.0 - ((epsilonK(k) - mu) / Ek)) * (1.0 + ((epsilonK(k) - mu) / Ek));
         // Guard ourselves against nasty NaNs
         if (Math.abs(underSqrt) < 1e-4) {
             underSqrt = 0;
@@ -118,12 +116,11 @@ public class Main {
      *
      * @param delta the energy gap
      * @param k     the wave vector.
-     * @param mass  the mass of the particle.
      * @param mu    the chemical potential.
      * @return the energy Ek.
      **/
-    static double eK(double delta, double k, double mass, double mu) {
-        return Math.sqrt(delta * delta + (epsilonK(k, mass) - mu) * (epsilonK(k, mass) - mu));
+    static double eK(double delta, double k, double mu) {
+        return Math.sqrt(delta * delta + (epsilonK(k) - mu) * (epsilonK(k) - mu));
     }
 
     /**
@@ -141,20 +138,19 @@ public class Main {
     /**
      * Calculate the energy gap from parameters.
      *
-     * @param ks   the wave vectors.
-     * @param eKs  the energies associated with wave vectors.
-     * @param mu   the chemical potential.
-     * @param mass the particle mass.
-     * @param T    the temperature.
+     * @param ks  the wave vectors.
+     * @param eKs the energies associated with wave vectors.
+     * @param mu  the chemical potential.
+     * @param T   the temperature.
      * @return the energy gap delta.
      **/
-    static double calcDelta(ArrayList<Double> ks, ArrayList<Double> eKs, double mass, double mu, double T) {
+    static double calcDelta(ArrayList<Double> ks, ArrayList<Double> eKs, double mu, double T) {
         // Arrays are needed for the summing
         double sum = 0;
         for (int i = 0; i < ks.size(); i++) {
             double k = ks.get(i);
             double eK = eKs.get(i);
-            sum += g * (uKvK(k, eK, mass, mu) * 0.5 * (fermiDirac(-eK, mu, T) - fermiDirac(eK, mu, T)));
+            sum += g * (uKvK(k, eK, mu) * 0.5 * (fermiDirac(-eK, mu, T) - fermiDirac(eK, mu, T)));
         }
         return sum;
     }
@@ -162,12 +158,11 @@ public class Main {
     /**
      * Calculate the energy gap until convergence.
      *
-     * @param mu   the chemical potential.
-     * @param mass the particle mass.
-     * @param T    the temperature.
+     * @param mu the chemical potential.
+     * @param T  the temperature.
      * @return the converged energy gap delta.
      **/
-    static double convergeDelta(double mass, double mu, double T) {
+    static double convergeDelta(double mu, double T) {
         // Take a guess
         double delta = 1.0;
         double deltaPrev = delta + 1e5; // Not to trigger the stop condition immediately
@@ -181,9 +176,9 @@ public class Main {
             double newN = 0;
             // Calculate the Ek's and Number of particles
             for (int i = 0; i < ks.size(); i++) {
-                eKs.add(eK(delta, ks.get(i), mass, mu));
-                newN += vK2(ks.get(i), eKs.get(i), mass, mu) * fermiDirac(-eKs.get(i), mu, T)
-                        + uK2(ks.get(i), eKs.get(i), mass, mu) * fermiDirac(eKs.get(i), mu, T);
+                eKs.add(eK(delta, ks.get(i), mu));
+                newN += vK2(ks.get(i), eKs.get(i), mu) * fermiDirac(-eKs.get(i), mu, T)
+                        + uK2(ks.get(i), eKs.get(i), mu) * fermiDirac(eKs.get(i), mu, T);
             }
             // On the first iteration we don't know what the N is, so dont mix the mu yet
             if (iterations != 0) {
@@ -193,16 +188,20 @@ public class Main {
             currentN = newN;
 
             // Get our new delta
-            delta = calcDelta(ks, eKs, mass, mu, T);
+            delta = calcDelta(ks, eKs, mu, T);
             if (Math.abs(deltaPrev - delta) < deltaPrecision) {
                 // We have converged - done
-                System.out.println("Delta converged after " + iterations + "   to " + delta);
+                System.out.printf("T: %.6f\tDelta: %.6f\t\t[took %d iterations]%n", T, delta, iterations);
+                nParticles = currentN;
                 return delta;
             } else {
-                // Mixed step that should speed up convergence, but it doesn't
-                // double alpha = 0.25;
-                // deltaPrev = alpha * delta + (1 - alpha) * deltaPrev;
-                deltaPrev = delta;
+                if (iterations > 100) {
+                    // There already have been lots of iterations -> mix
+                    double alpha = 0.25;
+                    deltaPrev = alpha * delta + (1 - alpha) * deltaPrev;
+                } else {
+                    deltaPrev = delta;
+                }
                 iterations++;
             }
         }
@@ -210,42 +209,58 @@ public class Main {
     }
 
     /**
-     * Draw the simulation plots.
+     * Draw and display the delta vs T and delta/Fermi Energy vs T plots.
      *
      * @param deltas the deltas corresponding the temperatures.
      * @param T      the temperatures.
      **/
     static void drawPlots(ArrayList<Double> deltas, ArrayList<Double> Ts) {
         // Construct the graph series
-        XYSeries xySeries = new XYSeries("Delta vs T");
+        XYSeries deltaTSeries = new XYSeries("Delta vs T");
         for (int i = 0; i < deltas.size(); i++) {
-            xySeries.add(Ts.get(i), deltas.get(i));
+            deltaTSeries.add(Ts.get(i), deltas.get(i));
+        }
+        double volume = Math.pow(nX * dx, 3);
+        double fermiE = h_bar * h_bar / (2 * mass);
+        fermiE *= Math.pow(3 * Math.PI * Math.PI * nParticles / volume, (double) 2 / 3);
+        XYSeries fermiDeltaTSeries = new XYSeries("Delta / Fermi energy vs T");
+        for (int i = 0; i < deltas.size(); i++) {
+            Double deltaOverFermiE = deltas.get(i) / fermiE;
+            fermiDeltaTSeries.add(Ts.get(i), deltaOverFermiE);
         }
 
         // Display the graphs
         ChartFrame frame1 = new ChartFrame("XYLine Chart", ChartFactory.createXYLineChart("Delta vs T", "Temperature",
-                "Delta value", new XYSeriesCollection(xySeries)));
+                "Delta value", new XYSeriesCollection(deltaTSeries)));
         frame1.setDefaultCloseOperation(ChartFrame.DISPOSE_ON_CLOSE);
         frame1.setLocationRelativeTo(null);
         frame1.setVisible(true);
         frame1.setSize(500, 400);
+        ChartFrame frame2 = new ChartFrame("XYLine Chart", ChartFactory.createXYLineChart("Delta / Fermi Energy vs T",
+                "Temperature", "Delta / Fermi Energy", new XYSeriesCollection(fermiDeltaTSeries)));
+        frame2.setDefaultCloseOperation(ChartFrame.DISPOSE_ON_CLOSE);
+        frame2.setLocationRelativeTo(null);
+        frame2.setVisible(true);
+        frame2.setSize(500, 400);
     }
 
     public static void main(String[] args) {
         ArrayList<Double> Ts = new ArrayList<Double>();
         ArrayList<Double> deltas = new ArrayList<Double>();
-        double mass = 1.0, mu = 1e-6, T = 1e-6; // T_0 = almost zero
+        double mu = 1e-6, T = 1e-6; // T_0 = almost zero
+        System.out.println("BEGIN SIMULATION");
+        System.out.println("----------------");
         establishKs();
         for (int i = 0; i < 1e7; i++) {
             T = T * 1.05;
             Ts.add(T);
-            deltas.add(convergeDelta(mass, mu, T));
+            deltas.add(convergeDelta(mu, T));
             if (deltas.get(i) <= almostZero) {
-                System.out.println("For T = " + T + " delta ~ 0");
-                System.out.println("Checked " + i + " different temperatures to match.");
+                System.out.printf("For T = %.6f delta ~ 0%n", T);
+                System.out.println("Checked " + i + " different temperatures.");
                 drawPlots(deltas, Ts);
                 double ratio = deltas.get(0) / T;
-                System.out.println("Ratio delta(T=0) / Tc supposed to be 1.76. In our case it is " + ratio);
+                System.out.printf("Ratio delta(T=0) / Tc: %.6f", ratio);
                 break;
             }
         }
